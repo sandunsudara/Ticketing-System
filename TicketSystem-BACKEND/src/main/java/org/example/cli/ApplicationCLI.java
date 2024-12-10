d47b8e9961d7b79c30afea9c33a6c109439d76df
Sandun Sudara <sandun_sudara@icet.lk>
2024-12-10 15:43:00 +0530

    add comment using java doc

--- TicketSystem-BACKEND/src/main/java/org/example/cli/ApplicationCLI.java
+++ TicketSystem-BACKEND/src/main/java/org/example/cli/ApplicationCLI.java
@@ -58,7 +58,10 @@ public class ApplicationCLI {
 
         }
     }
-    
+
+    /**
+     * Method use for prompt the user to system configuration details and writes them to a configuration file.
+     */
     public void configureSystem() {
         System.out.println("\n+----------------------------------+\n|         Configure System         |\n+----------------------------------+\n");
 
@@ -75,6 +78,12 @@ public class ApplicationCLI {
 
     }
 
+    /**
+     * Writes the system configuration to a JSON file.
+     *
+     * @param config The configuration object to be written to the file.
+     * @return A Response object indicating the result of the file write operation.
+     */
     public Response writeConfigToFile(Configuration config) {
         Gson gson = new Gson();
         gson.toJson(config, Configuration.class);
@@ -89,6 +98,11 @@ public class ApplicationCLI {
     }
 
 
+    /**
+     * Reads the system configuration from a JSON file.
+     *
+     * @return A Response object containing the configuration data or an error message.
+     */
     public Response<Configuration> readConfigFromFile() {
         Gson gson = new Gson();
         try (FileReader fileReader = new FileReader("src/main/java/org/example/util/config.json")) {
@@ -105,6 +119,12 @@ public class ApplicationCLI {
         }
     }
 
+    /**
+     * Get valid integer input from user.
+     *
+     * @param que The question or prompt displayed to the user.
+     * @return The valid integer input provided by the user.
+     */
     private int getValidInputForInteger(String que) {
         int value;
         while (true) {
@@ -123,6 +143,10 @@ public class ApplicationCLI {
         return value;
     }
 
+    /**
+     * Starts the simulation by initializing the ticket pool and generating customers and vendors.
+     * It also manages the simulation loop for adding new customers or vendors and stopping the simulation.
+     */
     public void simulationProgram() {
         // read config file
         Configuration configuration = readConfigFromFile().getData();
@@ -185,14 +209,26 @@ public class ApplicationCLI {
             System.out.println("Operation cancel");
         }
     }
-
+    /**
+     * Starts the simulation by starting the customer and vendor threads.
+     *
+     * @param customerThreads The list of customer threads to be started.
+     * @param vendorThreads   The list of vendor threads to be started.
+     */
     public void startSimulation(List<Thread> customerThreads, List<Thread> vendorThreads) {
         vendorThreads.forEach(Thread::start);
         customerThreads.forEach(Thread::start);
 
     }
 
-
+    /**
+     * Prompts the user for customer details and returns a list of customer threads based on the input.
+     *
+     * @param numberOfCustomer The number of customers to simulate.
+     * @param ticketPool       The ticket pool available for customers.
+     * @param customerRetrievalRate The rate at which customers retrieve tickets.
+     * @return A list of customer threads.
+     */
     private List<Thread> getCustomerDetail(int numberOfCustomer, TicketPool ticketPool, int customerRetrievalRate) {
         List<Thread> customerThreadList = new ArrayList<>();
         for (int i = 1; i <= numberOfCustomer; i++) {
@@ -221,6 +257,15 @@ public class ApplicationCLI {
         return customerThreadList;
     }
 
+    /**
+     * Generates a list of customer threads with randomly generated details.
+     * The number of tickets and VIP status are randomly assigned for each customer.
+     *
+     * @param numberOfCustomer The number of customer threads to generate.
+     * @param ticketPool The shared ticket pool used by all customers.
+     * @param customerRetrievalRate The rate at which customers will attempt to retrieve tickets.
+     * @return A list of customer threads.
+     */
     public List<Thread> autoGenerateCustomer(int numberOfCustomer, TicketPool ticketPool, int customerRetrievalRate) {
         List<Thread> customerThreadList = new ArrayList<>();
         for (int i = 1; i <= numberOfCustomer; i++) {
@@ -233,7 +278,17 @@ public class ApplicationCLI {
         return customerThreadList;
 
     }
-
+    
+    /**
+     * Generates a list of vendor threads with randomly generated details.
+     * Each vendor is assigned a unique ID and is provided with the configuration
+     * values for total tickets and ticket release rate.
+     *
+     * @param numberOfVendor The number of vendor threads to generate.
+     * @param ticketPool The shared ticket pool used by all vendors.
+     * @param config The configuration containing total tickets and ticket release rate.
+     * @return A list of vendor threads.
+     */
     public List<Thread> autoGenerateVendor(int numberOfVendor, TicketPool ticketPool, Configuration config) {
         List<Thread> vendorThreadList = new ArrayList<>();
         for (int i = 1; i <= numberOfVendor; i++) {
@@ -244,6 +299,11 @@ public class ApplicationCLI {
         return vendorThreadList;
     }
 
+    /**
+     * Sets the ticket pool for the application.
+     *
+     * @param ticketPool The ticket pool to set.
+     */
     public void setTicketPool(TicketPool ticketPool) {
         this.ticketPool = ticketPool;
     }
